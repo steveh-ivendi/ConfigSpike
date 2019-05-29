52408963833b9bb5ba5749edd81d7b0a2466bffd
Steve Hocking <steve.hocking@ivendi.com>
2019-05-29 09:24:28 +0100

    Add ConfigConsole to solution

---
+++ src/ConfigConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConfigConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
